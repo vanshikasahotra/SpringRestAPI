62ae5b25325fbac89ec81fbf69a79c543f2934a6
Vanshika Sahotra <65828435+vanshikasahotra@users.noreply.github.com>
2023-12-08 14:58:38 +0530

    Add files via upload

---
+++ spring2/src/learners.java
@@ -0,0 +1,10 @@
+public class learners {
+
+    private String learner_first_name;
+
+    private String learner_last_name;
+    private String learner_email;
+    private String password;
+    private int course_id;
+
+}
